cd7f3b4d01906c1b0ab4eada05c034cdebcaf887
8friend8ship-cloud <homedesigntaedi@gmail.com>
2026-08-23 12:30:57 +0900

    feat(monetization): initialize AdSense at frontend startup

--- index.tsx
+++ index.tsx
@@ -1,7 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installAdSense } from './services/adsense';
+
+installAdSense();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
